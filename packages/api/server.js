e0749af106d0edd9e6d71abe9b405347296a3ae3
Tiago Matos <tiagovolta@gmail.com>
2021-06-08 20:59:12 -0300

    feat: created commands to run on heroku

--- packages/api/server.js
+++ packages/api/server.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
-const app = require('./config/express')();
+const app = require("./config/express")();
 
-// RODANDO NOSSA APLICAÇÃO NA PORTA SETADA
 app.listen(process.env.PORT, () =>
   console.log(`Server runing on port ${process.env.PORT}!`)
 );
