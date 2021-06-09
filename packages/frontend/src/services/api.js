9aa2139837bf393f07fa79507a00ca63821c82a4
Tiago Matos <tiagovolta@gmail.com>
2021-06-08 21:03:14 -0300

    fix: changed baseUrl

--- packages/frontend/src/services/api.js
+++ packages/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/"
+  baseURL: "https://monorepo-2fa.herokuapp.com/"
 });
 
 api.interceptors.request.use(async config => {
